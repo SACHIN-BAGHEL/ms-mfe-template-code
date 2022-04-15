c991c6d6905287e90b3fd9724fadfad5dc024220
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2022-04-15 11:02:59 +0530

    Pagination Fix for Zero input valuel.

--- ui/widgets/widgets-dir/et-strapi-template-widget/src/components/TemplateDataTable.js
+++ ui/widgets/widgets-dir/et-strapi-template-widget/src/components/TemplateDataTable.js
@@ -99,7 +99,7 @@ class TemplateDataTable extends Component {
     }
 
     onSubmit = () => {
-        if (this.state.currPageWillUpdating && this.state.currPageWillUpdating <= this.state.pageSize) {
+        if (+this.state.currPageWillUpdating && this.state.currPageWillUpdating <= this.state.pageSize) {
             this.setState({page: +this.state.currPageWillUpdating})
         }
     };
