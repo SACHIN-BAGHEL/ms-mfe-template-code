105f2df74c5cc5bec077bf04729cb1a725f36e3e
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2022-04-26 14:03:47 +0530

    pficon-info and * Required Fields  placed in the order.

--- ui/widgets/widgets-dir/et-strapi-template-widget/src/components/ContentTemplateForm.js
+++ ui/widgets/widgets-dir/et-strapi-template-widget/src/components/ContentTemplateForm.js
@@ -496,7 +496,7 @@ class ContentTemplateForm extends Component {
         return (
             <div className="container-fluid" style={{ marginTop: "2vw" }}>
                 <form onSubmit={this.handleSubmit}>
-                    <div className="formContainer col-xs-12" style={{ marginBottom: "2vw" }}>
+                    <div className="formContainer col-xs-12" style={{ marginBottom: "2em" }}>
                         <div className="col-lg-6">
                             <h1 style={{ margin: "auto" }}><b>{this.props.formType === EDIT_LABEL ? EDIT_LABEL : ADD_LABEL}</b></h1>
                         </div>
@@ -511,24 +511,27 @@ class ContentTemplateForm extends Component {
                             {/* <button className="btn-primary primary-btn mv-2 btn" type="submit" disabled={!(this.state.errorObj.name.valid && this.state.errorObj.editorCoding.valid && this.state.errorObj.type.valid)} style={{ marginLeft:"1vw"}}>{SAVE_LABEL}</button> */}
                         </div>
                     </div>
+                    <div className='Required-field' style={{ marginBottom: "10px" }}>
+                        <span style={{
+                            paddingLeft: "1657px",
+                        }}>* Required Fields</span>
+                        <hr style={{ marginTop: "1px" }} />
+                    </div>
                     <div className="formContainer col-xs-12 form-group">
                         <div className="col-lg-1">
                             <label htmlFor="type" className="control-label">
                                 <span className="FormLabel">
-                                    <span>Type</span>
+                                    <span style={{
+                                        paddingLeft: "38px",
+                                        paddingTop: "2em"
+                                    }}>Type</span>
                                     <sup>
                                         <i className="fa fa-asterisk required-icon FormLabel__required-icon"></i>
                                     </sup>
-                                    <FieldLevelHelp
-                                        buttonClass=""
-                                        close={undefined}
-                                        content="Select one existing collection type to use for the content template."
-                                        inline
-                                        placement="right"
-                                        rootClose
-                                    />
-                                    {/* <button type="button" className="btn btn-link">
-        <span aria-hidden="true" className="pficon pficon-info"></span>
+                                    <FieldLevelHelp buttonClass="" close={undefined} content="Select one existing collection type to use for the content template." inline placement="right" rootClose />
+                                    {/* <button type="button" className="btn btn-link" >
+        <span aria-hidden="true" className="pficon pficon-info" content="Select one existing collection type to use for the content template.">
+        </span>
         </button> */}
                                 </span>
                             </label>
@@ -559,18 +562,11 @@ class ContentTemplateForm extends Component {
                         <div className="col-lg-1">
                             <label htmlFor="name" className="control-label">
                                 <span className="FormLabel">
-                                    <span>Name</span>
+                                    <span style={{ paddingLeft: "32px" }}>Name</span>
                                     <sup>
                                         <i className="fa fa-asterisk required-icon FormLabel__required-icon"></i>
                                     </sup>
-                                    <FieldLevelHelp
-                                        buttonClass=""
-                                        close={undefined}
-                                        content="You can insert up to 50 characters, including upper or lowerccase letters, numbers and special characters."
-                                        inline
-                                        placement="right"
-                                        rootClose
-                                    />
+                                    <FieldLevelHelp buttonClass="" close={undefined} content="You can insert up to 50 characters, including upper or lowerccase letters, numbers and special characters." inline placement="right" rootClose />
                                     {/* <button type="button" className="btn btn-link">
         <span aria-hidden="true" className="pficon pficon-info"></span>
         </button> */}
@@ -600,21 +596,14 @@ class ContentTemplateForm extends Component {
                         </div>
                     </div>
                     <div className="formContainer col-xs-12 form-group">
-                        <div className="col-lg-1" style={{padding: "0"}}>
+                        <div className="col-lg-1">
                             <label htmlFor="attributes" className="control-label">
                                 <span className="FormLabel">
-                                    <span>Attributes</span>
+                                    <span style={{ paddingLeft: "8px" }}> Attributes</span>
                                     <sup>
                                         <i className="fa fa-asterisk required-icon FormLabel__required-icon"></i>
                                     </sup>
-                                    <FieldLevelHelp
-                                        buttonClass=""
-                                        close={undefined}
-                                        content="Provides the attributes list for the selected collection type."
-                                        inline
-                                        placement="right"
-                                        rootClose
-                                    />
+                                    <FieldLevelHelp buttonClass="" close={undefined} content="Provides the attributes list for the selected collection type." inline placement="right" rootClose />
                                 </span>
                             </label>
                         </div>
@@ -643,23 +632,21 @@ class ContentTemplateForm extends Component {
                         <div className="col-lg-1">
                             <label htmlFor="modal" className="control-label">
                                 <span className="FormLabel">
-                                    <span>Model</span>
+                                    <span style={{ paddingLeft: "30px" }}>Model</span>
                                     <sup>
                                         <i className="fa fa-asterisk required-icon FormLabel__required-icon"></i>
                                     </sup>
-                                    <FieldLevelHelp
-                                        buttonClass=""
-                                        close={undefined}
-                                        content="Defines the HTML content structure using the content elements defined by the given content type."
-                                        inline
-                                        placement="right"
-                                        rootClose
-                                    />
+                                    <FieldLevelHelp buttonClass="" close={undefined} content="Defines the HTML content structure using the content elements defined by the given content type." inline placement="right" rootClose />
                                 </span>
                             </label>
                         </div>
                         <div className="col-lg-11">
-                            <button type="button" onClick={() => this.setState({ modalShow: true })} className="btn btn-success">Inline editing assistant</button>
+                            <button type="button" onClick={() => this.setState({ modalShow: true })} className="btn-default btn" style={{
+                                // backgroundColor : "#cccccc",
+                                color: "black"
+
+                                // <button className="btn-default btn">{CANCEL_LABEL}</button>
+                            }}>Inline editing assistant</button>
                         </div>
                     </div>
                     <div className="formContainer col-xs-12 form-group">
@@ -706,21 +693,14 @@ class ContentTemplateForm extends Component {
                         </div>
                     </div>
                     <div className="formContainer col-xs-12 form-group">
-                        <div className="col-lg-1" style={{padding: "0"}}>
+                        <div className="col-lg-1">
                             <label htmlFor="stylesheet" className="control-label">
                                 <span className="FormLabel">
-                                    <span>Style Sheet</span>
+                                    <span style={{ paddingLeft: "8px" }}>Style Sheet</span>
                                     {/* <sup>
         <i className="fa fa-asterisk required-icon FormLabel__required-icon"></i>
         </sup> */}
-                                    <FieldLevelHelp
-                                        buttonClass=""
-                                        close={undefined}
-                                        content="Provides a stylesheet file to be used with the HTML model."
-                                        inline
-                                        placement="right"
-                                        rootClose
-                                    />
+                                    <FieldLevelHelp buttonClass="" close={undefined} content="Provides a stylesheet file to be used with the HTML model." inline placement="right" rootClose />
                                     {/* <button type="button" className="btn btn-link">
         <span aria-hidden="true" className="pficon pficon-info"></span>
         </button> */}
